0222719968d1e4522d725c289171256942186044
Arkadiy <agongadze8@gmail.com>
2025-03-31 22:54:05 +0300

    Небольшие изменения

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from users.models import User
+
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    search_fields = ['username', 'email']
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    ordering = ('username',)
